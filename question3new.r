6c856a98c1c54ca3ae6c7a8d6c55bc02cda29d44
inakaria <inakaria1364@gmail.com>
2024-05-03 15:36:04 -0400

    Final changes

--- question3new.r
+++ question3new.r
@@ -1,22 +1,37 @@
 library(ggplot2)
 library(dplyr)
+library(cowplot)
 
 png(filename = "NEWQ3 - Number of Terrorist Attacks by Type.png", 
-    width = 2500, height = 2500, res = 300)
+    width = 5000, height = 2000, res = 300)
 data <- read.csv("terror.csv")
 
-data_grouped <- data %>% 
+data_succ <- data %>% 
                 filter(AttackType != "Unknown") %>%
                 group_by(Year, AttackType) %>% 
-                summarise(Total_events = n(), .groups = 'drop') %>% ungroup()
+                summarise(Successful_events = sum(Success), .groups = 'drop') %>% ungroup()
+
+data_notsucc <- data %>% 
+                filter(AttackType != "Unknown") %>%
+                group_by(Year, AttackType) %>% 
+                summarise(Total_events = n()-sum(Success), .groups = 'drop') %>% ungroup()
                 
-question3 <- ggplot(data = data_grouped, aes(x = Year, y = Total_events, colour = AttackType)) + 
+question31 <- ggplot(data = data_succ, aes(x = Year, y = Successful_events, colour = AttackType)) + 
+    geom_line() +
+    geom_point() +
+    labs(title = "Number of Successful Terrorist Attacks by Type",
+         x = "Year",
+         y = "Number of Successful Attacks") +
+    theme_gray()
+
+question32 <- ggplot(data = data_notsucc, aes(x = Year, y = Total_events, colour = AttackType)) + 
     geom_line() +
     geom_point() +
     labs(title = "Number of Terrorist Attacks by Type",
          x = "Year",
-         y = "Number of Attacks") +
+         y = "Number of Unsuccessful Attacks") +
     theme_gray()
 
-print(question3)
+combined_plot <- plot_grid(question31, question32, ncol=2) 
+print(combined_plot)
 dev.off()
